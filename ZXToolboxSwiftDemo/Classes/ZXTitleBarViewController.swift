03eb8492a7e5ac9bbf83ce7c44ac476e8dead926
zhaoxin <zhaoxin@meishubao.com>
2021-12-02 18:08:26 +0800

    ZXTitleBarViewController

--- ZXToolboxSwiftDemo/Classes/ZXTitleBarViewController.swift
+++ ZXToolboxSwiftDemo/Classes/ZXTitleBarViewController.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 
-class ZXTitleBarViewController: UIViewController {
+class ZXTitleBarViewController: ZXTitleBarController {
+    @IBOutlet weak var tableView: UITableView!
+
+    lazy var backButton = UIButton(type: .infoLight)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.leftBarButtonView = backButton
+        self.scrollViewDidScroll(tableView, offset: 0, length: 300, defaultStyle: .default, opacityStyle: .default)
+        //
+        backButton.addTarget(self, action: #selector(onButton(_:)), for: .touchUpInside)
+    }
+    
+    @objc func onButton(_ sender: Any?) {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+extension ZXTitleBarViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let r = CGFloat(arc4random() % 100) / 100.0
+        let g = CGFloat(arc4random() % 100) / 100.0
+        let b = CGFloat(arc4random() % 100) / 100.0
+        cell.contentView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        return cell
     }
 }
